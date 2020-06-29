7730b15ed09ed294f177e8ab8e770df6a4eb39db
qiuling <qiuling@baijiayun.com>
2020-06-29 10:51:17 +0800

    feat(module)增加启动默认theme 对启动速度没有什么增加 但是可以增加用户体验

--- app/src/main/java/com/dsg/androidperformance/MainActivity.java
+++ app/src/main/java/com/dsg/androidperformance/MainActivity.java
@@ -35,6 +35,7 @@ public class MainActivity extends AppCompatActivity implements OnFeedShowCallBac
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mRecyclerView = findViewById(R.id.recycler_view);
