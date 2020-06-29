fe3d899496b32b001e5fc1338fce04d079622b77
qiuling <qiuling@baijiayun.com>
2020-06-29 15:06:14 +0800

    feat(module)增加生成的traceview

--- app/src/main/java/com/dsg/androidperformance/tasks/GetDeviceIdTask.java
+++ app/src/main/java/com/dsg/androidperformance/tasks/GetDeviceIdTask.java
@@ -1,7 +1,14 @@
 package com.dsg.androidperformance.tasks;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.dsg.androidperformance.PerformanceApp;
 import com.dsg.androidperformance.launcherStarter.task.Task;
@@ -15,7 +22,7 @@ public class GetDeviceIdTask extends Task {
         // 真正自己的代码
         TelephonyManager tManager = (TelephonyManager) mContext.getSystemService(
                 Context.TELEPHONY_SERVICE);
-        mDeviceId = tManager.getDeviceId();
+        mDeviceId = "2121312312321312312312312";
         PerformanceApp app = (PerformanceApp) mContext;
         app.setDeviceId(mDeviceId);
     }
